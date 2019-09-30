9ba4782042a99f827e899412537126c3414538c9
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-09-30 10:58:53 -0500

    Add redux store

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import App from 'next/app'
+import { Provider } from 'react-redux'
+import withReduxStore from './../lib/withReduxStore';
+
+class MyApp extends App {
+  // Only uncomment this method if you have blocking data requirements for
+  // every single page in your application. This disables the ability to
+  // perform automatic static optimization, causing every page in your app to
+  // be server-side rendered.
+  //
+  // static async getInitialProps(appContext) {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await App.getInitialProps(appContext);
+  
+  //   return { ...appProps }
+  // }
+
+  render() {
+    const { Component, pageProps, reduxStore } = this.props
+    return (
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    )
+  }
+}
+
+export default withReduxStore(MyApp)
