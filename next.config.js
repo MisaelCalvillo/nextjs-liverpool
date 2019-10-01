76cd0667bb7cc1970d649c4f62ccbde5343698c3
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-10-01 11:19:45 -0500

    Cofigure weback

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-const withStylus = require('@zeit/next-stylus')
+const withStylus = require('@zeit/next-stylus');
+require('dotenv').config();
+const webpack = require('webpack');
 
-module.exports = withStylus({
-  cssModules: true
-})
+module.exports = {
+  webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    console.log('QUE ES ENV', env);
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+
+    return config;
+  }
+}
